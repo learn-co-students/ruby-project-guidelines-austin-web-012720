bc233cf76a03b0c97481a67841482e5e2100f48a
lughjw <lughjw@gmail.com>
2020-02-10 17:19:18 -0600

    cleaned up app/models/challenge.rb logic.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,16 +2,16 @@ class Challenge < ActiveRecord::Base
     belongs_to :encounter
     has_many :locations, through: :encounters
 
-    def take_damage
-        challenge.health -= spell.damage - challenge.health
+    def take_damage(damage)
+        self.health -= damage - self.armor
     end
 
-    def check_visibility
-        challenge.stealth
+    def visibile?
+        !self.stealth
     end
 
-    def engage_element
-        if challenge.element == "fire"
+    def engage_element(spell)
+        if self.element == "fire"
             if spell.element == "fire"
                 spell.damage = 0
             end
@@ -19,7 +19,7 @@ class Challenge < ActiveRecord::Base
                 spell.damage *= 2
             end
         end
-        if challenge.element == "water"
+        if self.element == "water"
             if spell.element == "water"
                 spell.damage = 0
             end
@@ -29,16 +29,16 @@ class Challenge < ActiveRecord::Base
         end
     end
 
-    def check_and_engage_element
-        if challenge.element 
-            engage_element
+    def check_and_engage_element(element)
+        if self.element 
+            engage_element(element)
         end
     end
 
-    def recieve_spell(spell)
-        if check_visibility
-            check_and_engage_element
-            take_damage
+    def receive_spell(spell)
+        if self.visibile?
+            check_and_engage_element(spell)
+            take_damage(spell.damage)
         else
             print "There is nothing to target!"
         end
