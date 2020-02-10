9832195274b65d79430658983014be96a568ea25
lughjw <lughjw@gmail.com>
2020-02-10 13:24:58 -0600

    Added cast_spell to spellbot. Added recieve_spell to challenge.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,8 @@
 class Challenge < ActiveRecord::Base
     belongs_to :instance
     has_many :locations, through: :instance
+
+    def recieve_spell(spell)
+        
+    end
 end
--- app/models/spellbot.rb
+++ app/models/spellbot.rb
@@ -2,4 +2,12 @@ class Spellbot < ActiveRecord::Base
     has_many :spells
     has_many :challenges, through: :instances
     has_many :locations, through: :instances
+
+    def cast_spell(spell_name, target_name)
+        spell = spells.find_by_name(spell_name)
+        target = challenges.find_by_name(target_name)
+        # will modify based on attributes later
+        target.recieve_spell(spell)
+
+    end
 end
