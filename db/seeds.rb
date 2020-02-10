2b582a53908f58fd92a0153b88383d33582b027f
lughjw <lughjw@gmail.com>
2020-02-10 17:36:32 -0600

    changed seeds.db to call create instead of calling new

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-bush = Challenge.new(name: "A Bush", description:"Just...a bush. Why do you need to kill a bush? You monster.", health: 3, stealth: 0, armor: 0)
-fieldod = Location.new(name: "Field of Despair", description: "Behold the Field of Despair! Marvel at the terrifying and dangerous challenges you must overcome to progress.")
-#instance 1 ^^^
+bush = Challenge.create(name: "A Bush", description:"Just...a bush. Why do you need to kill a bush? You monster.", health: 3, stealth: 0, armor: 0)
+fieldod = Location.create(name: "Field of Despair", description: "Behold the Field of Despair! Marvel at the terrifying and dangerous challenges you must overcome to progress.")
+#encounter 1 ^^^
 
-bat = Challenge.new(name: "b-ATTR-ACCESSOR", description: "Hidden in the darkness you see it: due to the turbulent mana emanating deep within the mountain these bats have become influenced by the attr_accessor...they can give just as well as they can take, be careful!", health: 5, stealth: 1, armor: 0)
-emptycave = Location.new(name: "Empty(?) Cave", description: "Take a moment to rest in this empty cave...wait...do you hear that? Let's take a closer look...")
-#instance 2 ^^
+bat = Challenge.create(name: "b-ATTR-ACCESSOR", description: "Hidden in the darkness you see it: due to the turbulent mana emanating deep within the mountain these bats have become influenced by the attr_accessor...they can give just as well as they can take, be careful!", health: 5, stealth: 1, armor: 0)
+emptycave = Location.create(name: "Empty(?) Cave", description: "Take a moment to rest in this empty cave...wait...do you hear that? Let's take a closer look...")
+#encounter 2 ^^
