f7a58ed564012c0994766d2df10d3cedae7d4bcb
github username <github email address>
2020-02-11 09:55:02 -0600

    Created login funtionality with password validations

--- lib/category.rb
+++ lib/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
     has_many :user_categories 
     has_many :users, through: :user_categories
     has_many :category_questions
+
+    def self.get_category
+        # response = 
+    end
 end
