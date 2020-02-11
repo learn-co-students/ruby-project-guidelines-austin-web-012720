0a8c4b76455cfae89fb9ab041fd7e2151fc9791f
lughjw <lughjw@gmail.com>
2020-02-11 13:38:36 -0600

    simplified element check in challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,7 +3,11 @@ class Challenge < ActiveRecord::Base
     has_many :locations, through: :encounters
 
     def take_damage(damage)
-        self.health -= damage - self.armor
+        if self.armor < damage
+            self.health -= damage - self.armor
+        end
+
+        self.health
     end
 
     def visible?
@@ -11,34 +15,25 @@ class Challenge < ActiveRecord::Base
     end
 
     def engage_element(spell)
-        if self.element == "fire"
-            if spell.element == "fire"
-                spell.damage = 0
-            end
-            if spell.element == "water"
-                spell.damage *= 2
-            end
-        end
-        if self.element == "water"
-            if spell.element == "water"
-                spell.damage = 0
-            end
-            if spell.element == "fire"
-                spell.damage *= 2
-            end
+        # works for now, but when we have more than 2 elements we'll need to change this
+        # currently "fire", "water", and nil are the only values for element
+        if self.element == spell.element
+            spell.damage = 0
+        else
+            spell.damage *= 2
         end
     end
 
-    def check_and_engage_element(element)
-        if self.element 
-            engage_element(element)
+    def check_and_engage_element(spell)
+        if self.element
+            engage_element(spell)
         end
     end
 
     def receive_spell(spell)
         if self.visibile?
             check_and_engage_element(spell)
-            take_damage(spell.damage)
+            self.take_damage(spell.damage)
         else
             print "There is nothing to target!"
         end
--- app/models/spellbot.rb
+++ app/models/spellbot.rb
@@ -5,8 +5,8 @@ class Spellbot < ActiveRecord::Base
     has_many :locations, through: :encounters
 
     def cast_spell(spell_name, target_name)
-        spell = spells.find_by_name(spell_name)
-        target = challenges.find_by_name(target_name)
+        spell = spells.find_by(name: spell_name)
+        target = challenges.find_by(name: target_name)
         # will modify based on attributes later
         target.recieve_spell(spell)
 
