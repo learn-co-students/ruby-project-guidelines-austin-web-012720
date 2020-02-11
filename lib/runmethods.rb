fa98711f43d61eaecef787ea3f7898e24b36d8ce
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-02-11 11:48:14 -0600

    runner file cleaned up and title screen more polished

--- lib/runmethods.rb
+++ lib/runmethods.rb
@@ -1,4 +1,17 @@
 def game_startup
-    PROMPT.yes?('Is this your first time playing?', help_color: :red, active_color: :green)
-    puts "Who cares! We don't have enough content for that to matter right now!".colorize(:red)
+    PROMPT.say("
+    +-+ +-+ +-+ +-+ +-+ +-+ +-+    
+    |W| |E| |L| |C| |O| |M| |E|    
+    +-+ +-+ +-+ +-+ +-+ +-+ +-+    
+        |-| |T| |O| |-|        
+    +-+ +-+ +-+ +-+ +-+ +-+    
+    |~| |R| |U| |B| |Y| |~|    
+    +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
+    |~| |W| |I| |Z| |A| |R| |D| |~|
+    +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
+
+
+    ", color: :bright_red)
+    PROMPT.yes?('Is this your first time playing?', help_color: :bright_red, active_color: :green)
+    PROMPT.say("Who cares! We don't have enough content for that to matter right now!", color: :bright_red)
 end
