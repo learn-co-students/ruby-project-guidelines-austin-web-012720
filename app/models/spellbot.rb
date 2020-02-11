a723a8800ebf8ad6fb3f67d4ddc7962beed7f0bd
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-02-11 13:38:01 -0600

    created spellbot method for spell selection, changed element values to nil instead of 'none'

--- app/models/spellbot.rb
+++ app/models/spellbot.rb
@@ -4,11 +4,17 @@ class Spellbot < ActiveRecord::Base
     has_many :challenges, through: :encounters
     has_many :locations, through: :encounters
 
-    def cast_spell(spell_name, target_name)
-        spell = spells.find_by_name(spell_name)
-        target = challenges.find_by_name(target_name)
+    def cast_spell(spell_name)
+        spell = Spells.find_by_name(spell_name)
+        target = Challenges.find_by(id: encounter.id)
         # will modify based on attributes later
         target.recieve_spell(spell)
 
+    end
+
+    def spell_prompt
+        spell = PROMPT.select("Pick which spell to cast", %w(Manabolt Inspect Frostbolt), active_color: :bright_red, per_page: 6)
+        PROMPT.say("You cast #{spell}!", color: :blue)
+        cast_spell(spell)
     end
 end
