6bdaaa31fdb9d454ad20ba90fa951903165449e0
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-02-11 14:45:18 -0600

    runmethods for end_screen and reconfig game_intro

--- lib/runmethods.rb
+++ lib/runmethods.rb
@@ -6,13 +6,57 @@ def title_logo
     puts PASTEL.red(FONT.write("^^^^^^^^^^^^", letter_spacing: 2))
 end
 
+def game_intro
+    PROMPT.say("Welcome to ", color: :green) 
+    PROMPT.say("RUBY WIZARD ", color: :bright_red)
+    PROMPT.say("!", color: :green) 
+    PROMPT.yes?('Is this your first time playing?', help_color: :green, active_color: :green)
+    PROMPT.say("Who cares! We don't have enough content for that to matter right now!", color: :green)
+    puts " "
+    puts " "
+    PROMPT.say("Ruby wizard will be an educational text-based game focused on Ruby as a programming language", color: :bright_red)
+    PROMPT.say("Please enjoy what will hopefully be a pleasant experience, and apologies if you break the code...", color: :bright_red)
+    puts ""
+    PROMPT.say(". . . . . . . . . .", color: :red)
+    PROMPT.say(". . . . . . . . . .", color: :red)
+    puts ""
+    PROMPT.say(". . . . . . . . . .", color: :red)
+    puts ""
+    PROMPT.say(". . . . . . . . . .", color: :red)
+    PROMPT.say(". . . . . . . . . .", color: :red)
+    puts ""
+    PROMPT.say("...but you probably deserve it.", color: :bright_red)
+    puts ""
+    puts ""
+    PROMPT.yes?('Are you ready to start?', help_color: :green, active_color: :green)
+    puts ""
+    puts ""
+    PROMPT.say("Doesn't matter, let's go!", color: :bright_red)
+    puts ""
+    puts ""
+    puts ""
+
+end
+
+def end_screen
+    PROMPT.say("As you stumble through the clearing ahead, you notice...nothing. Absolutely nothing for you to fight or do.", color: :bright_red)
+    PROMPT.ask("Did...did you win?")
+    PROMPT.say("You did!!", color: :bright_red)
+    PROMPT.ask("Press enter to win!")
+    puts PASTEL.red(FONT.write(". . . . . . . . . . .", letter_spacing: 3))
+    puts PASTEL.bright_red(FONT.write(" YOU   HAVE", letter_spacing: 2))
+    puts PASTEL.bright_red(FONT.write("CONQUERED", letter_spacing: 2))
+    puts PASTEL.bright_red(FONT.write("THIS ALPHA", letter_spacing: 2))
+    puts PASTEL.red(FONT.write("^^^^^^^^^^^^^^", letter_spacing: 2))
+end
+
 def game_startup
     title_logo
-    #PROMPT.yes?('Is this your first time playing?', help_color: :bright_red, active_color: :bright_red)
-
-    PROMPT.say("Who cares! We don't have enough content for that to matter right now!", color: :bright_red)
- 
+    game_intro
+    end_screen
 end
+
+
 
 def spell_prompt
     spell = PROMPT.select("Pick which spell to cast", %w(ManaBolt Inspect FrostBolt), active_color: :bright_red, per_page: 6)
